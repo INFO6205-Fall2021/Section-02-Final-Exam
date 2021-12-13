6a3ee62e984bcf5ac6eae64eebea7669f980fc0c
Oliver Rodrigues <oliverrodrigues996@gmail.com>
2021-12-13 16:56:43 -0500

    upadet

--- src/main/java/neu/info6205/edu/Solution.java
+++ src/main/java/neu/info6205/edu/Solution.java
@@ -2,7 +2,7 @@ package neu.info6205.edu;
 import java.util.List;
 
 public class Solution {
-    public int connectedSum(int graph_nodes, List<Integer> graph_from, List<Integer> graph_to){
+    public int connectedSquareSum(int graph_nodes, List<Integer> graph_from, List<Integer> graph_to){
 
         return 0;
     }
